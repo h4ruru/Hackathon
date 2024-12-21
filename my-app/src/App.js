84d24fbdb173d577dd5f6fbfbbae8138e458c0b6
y-semba <senba1008basen@icloud.com>
2024-12-21 16:39:12 +0900

    検索画面と登録画面の作成

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,12 @@
 
 import './App.css';
+import Distribute from './components/body_dis';
+import Resister from './components/body_regi'
 
 function App() {
   return (
     <>
+      <Resister />
     </>
   );
 }
