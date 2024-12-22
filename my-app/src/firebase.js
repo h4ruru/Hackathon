84d6128e95a99aa8cbd6d54b079c74f84eeb82f1
h4ruru <haru@katsube.net>
2024-12-22 19:26:01 +0900

    データベース締め

--- my-app/src/firebase.js
+++ my-app/src/firebase.js
@@ -1,15 +1,15 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-const firebaseConfig = {
-  apiKey: "AIzaSyCmqPGZPbgm844rYoyx1py8kBgcd7xZrPE",
-  authDomain: "hackathon1-f4207.firebaseapp.com",
-  projectId: "hackathon1-f4207",
-  storageBucket: "hackathon1-f4207.firebasestorage.app",
-  messagingSenderId: "845494579346",
-  appId: "1:845494579346:web:31f4eb6f457b503273764b"
-};
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+// const firebaseConfig = {
+//   apiKey:
+//   authDomain:
+//   projectId:
+//   storageBucket:
+//   messagingSenderId:
+//   appId:
+// };
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app);
 
-export { app, db};
+// export { app, db};
